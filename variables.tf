2af133b310e34d1f65ac3ce0e729599505d76196
Antonio Castro <antoniocauk@gmail.com>
2022-03-23 11:02:05 +0100

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {}
+variable "region" {}
+variable "zone" {}
+
+variable "gcs_transfer" {}
+
+variable "schedule_year" {}
+variable "schedule_month" {}
+variable "schedule_day" {}
+
+variable "end_bucket_name" {}
